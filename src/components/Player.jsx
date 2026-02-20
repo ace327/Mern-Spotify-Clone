e7370ce54218b3b85abe4db3ccd73fe5d1e5df46
ace327 <aryasuthar2002@gmail.com>
2026-02-21 01:03:46 +0530

    t

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -13,7 +13,34 @@ const Player = () => {
             </div> 
         </div>
 
-        <div className="flex flex-col item-center gap-1"></div>
+        <div className="flex flex-col item-center gap-1 m-auto">
+
+            <div className="flex gap-4">
+
+
+            <img src= {assets.shuffle_icon} alt="" className="w-4 cursor-pointer" />
+            <img src= {assets.prev_icon} alt="" className="w-4 cursor-pointer" />
+            <img src= {assets.play_icon} alt="" className="w-4 cursor-pointer" />
+            <img src= {assets.next_icon} alt="" className="w-4 cursor-pointer" />
+            <img src= {assets.loop_icon} alt="" className="w-4 cursor-pointer" />
+            </div>
+            <div className="flex items-center gap-5">
+                <p>1:06</p>
+                <div className="w-[60vh] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
+                    <hr className="h-1 border-none w-0 bg-green-800 rounded-full" />
+                </div>
+                <p>3:20</p>
+            </div>
+        </div>
+
+        <div className="hidden lg:flex items-center gap-2 opacity-75">
+            <img src={assets.play_icon} alt="" className="w-4" />
+            <img src={assets.mic_icon} alt="" className="w-4" />
+            <img src={assets.queue_icon} alt="" className="w-4" />
+            <img src={assets.speaker_icon} alt="" className="w-4" />
+            <img src={assets.volume_icon} alt="" className="w-4" />
+            <div className="w-20 bg-state-50 h-1 rounded"></div>
+        </div>
       
     </div>
   )
