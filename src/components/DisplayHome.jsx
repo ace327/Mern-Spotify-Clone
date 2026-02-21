ad2b829d14c3193dc1ba7a2af18329993d3bb291
ace327 <aryasuthar2002@gmail.com>
2026-02-21 21:14:19 +0530

    t

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const DisplayHome = () => {
   return (
     <div>
-      
+      <Navbar />
+      <div className="qmy-5 font-bold text-2xl"></div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Navbar = () => {
-  return (
+  return ( 
     <div className='w-full flex justify-between items-center font-semibold '>
         <div className="flex items-cener gap-2">
             <img src={assets.arrow_left} alt="" className="w-8 bg-black p-2 rounded-2xl cursor-pointer" />
@@ -12,7 +12,17 @@ const Navbar = () => {
         <div className="flex items-center gap-4">
             <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2x1 hidden md:block  cursor-pointer"></p>
             <p className="bg-black p-1 px-3 rounded-2xl text-[15px] cursor-pointer">Install App</p>
+            <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center jutify-center ">A</p>
         </div>
+        <div className="flex items-center gap-2 mt-4">
+            <p className='bg-white text-black px-4 py-1 rounded-2xl'>All</p>
+            <p className='bg-black px-4py-1 rounded-2xl'>Music</p>
+            <p className='bg-black px-4py-1 rounded-2xl'>Podcast</p>
+
+
+
+        </div>
+
       
     </div>
   )
