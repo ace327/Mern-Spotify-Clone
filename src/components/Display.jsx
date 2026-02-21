27b73ce6ecba0e6d16ba4061b149052c4543a456
ace327 <aryasuthar2002@gmail.com>
2026-02-21 11:48:32 +0530

    t

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import DisplayHome from './DisplayHome'
 
 const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 '>
+        <Router>
+            <Route path='/' element={<DisplayHome/>}/>
+        </Router>
       
     </div>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='w-full flex justify-between items-center font-semibold '>
+        <div className="flex items-cener gap-2">
+            <img src={assets.arrow_left} alt="" className="w-8 bg-black p-2 rounded-2xl cursor-pointer" />
+            <img src={assets.arrow_right} alt="" className="w-8 bg-black p-2 rounded-2xl cursor-pointer" />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
