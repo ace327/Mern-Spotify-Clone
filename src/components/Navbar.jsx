b6d15ecdd6f3fa9388b29bda2b09317a932201f8
ace327 <aryasuthar2002@gmail.com>
2026-02-21 15:44:45 +0530

    t

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,11 @@ const Navbar = () => {
             <img src={assets.arrow_left} alt="" className="w-8 bg-black p-2 rounded-2xl cursor-pointer" />
             <img src={assets.arrow_right} alt="" className="w-8 bg-black p-2 rounded-2xl cursor-pointer" />
         </div>
+
+        <div className="flex items-center gap-4">
+            <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2x1 hidden md:block  cursor-pointer"></p>
+            <p className="bg-black p-1 px-3 rounded-2xl text-[15px] cursor-pointer">Install App</p>
+        </div>
       
     </div>
   )
