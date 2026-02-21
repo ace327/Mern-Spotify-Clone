8a6bd6d9e1e9a3d7978fa59432a8d645d36e789b
ace327 <aryasuthar2002@gmail.com>
2026-02-22 00:40:08 +0530

    t

---
+++ src/components/AlbumItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AlbumItem = ({image , name , desc, id }) => {
+  return (
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer '>
+      
+    </div>
+  )
+}
+
+export default AlbumItem
