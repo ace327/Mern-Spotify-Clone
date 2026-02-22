362b2bddfe3d583487ef44472f5f3919c239c216
ace327 <aryasuthar2002@gmail.com>
2026-02-22 21:22:43 +0530

    t

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { albumsData } from '../assets/assets'
 
 const SongItem = () => {
   return (
     <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
     <p className="text-white">
         <b className="mr-4 text-[#a7a7a7]">{index+1}</b>
-        <img src={} alt="" />
-        </p>  
+        <img src={item.image} alt="" />
+        </p> 
+        <p>{album.item}</p>
+        
     </div>
   )
 }
