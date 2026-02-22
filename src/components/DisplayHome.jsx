196161e26a394dbdf25328c44d7b3b7203a48a21
ace327 <aryasuthar2002@gmail.com>
2026-02-22 20:43:32 +0530

    t

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
 
 const DisplayHome = () => {
   return (
     <div>
       <Navbar />
-      <div className="qmy-5 font-bold text-2xl"></div>
+      <div className="mb-4">
+        <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+        <div className="flex overflow-auto"> </div>
+        {albumsData.map((item,index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+      </div>
+      <div className="mb-4">
+        <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
+        <div className="flex overflow-auto"> </div>
+        {albumsData.map((item,index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+      </div>
     </div>
   )
 }
---
+++ src/components/SongItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SongItem = () => {
+  return (
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <p className="text-white">
+        <b className="mr-4 text-[#a7a7a7]">{index+1}</b>
+        <img src={} alt="" />
+        </p>  
+    </div>
+  )
+}
+
+export default SongItem
