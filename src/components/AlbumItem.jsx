408da1d188fb2ef39f7d807a3045fa0011645220
ace327 <aryasuthar2002@gmail.com>
2026-03-03 03:41:22 +0530

    jai shree krishna

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 const AlbumItem = ({image , name , desc, id }) => {
   return (
+
+   
+
+
     <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
         <img src={image} className='rounded'  alt="" />
         <p className="font-bold mt-2 mb-1">{name} </p>
