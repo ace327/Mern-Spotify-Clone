0a99b4bcb655874683b3573747cfa43123fdb43c
ace327 <aryasuthar2002@gmail.com>
2026-03-08 04:42:37 +0530

    jai shree krishna

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -13,7 +13,7 @@ const Player = () => {
   return (
     <div className='h-[10%] bg-black flex  justify-between items-center text-white px-4'>
         <div className="hidden lg:flex items-center gap-4">
-            <img className='w-12' src={songsData[0].image} alt="" />
+            <img className='w-12' src={track?.image} alt="" />
             <div> 
                 <p>{track?.name}</p>
                 <p>{track?.desc.slice(0,12)}</p>
